3db99feeb99e73dac32cdfcc9573348335e9ebf5
Larry Clement <lclement@calbaptist.edu>
2022-01-19 20:48:47 -0800

    Adding project files

---
+++ LoopDemo/src/Loop1.java
@@ -0,0 +1,8 @@
+public class Loop1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("I am so smart " + i);
+        }
+//        System.out.println(i);
+    }
+}
---
+++ LoopDemo/src/Loop2.java
@@ -0,0 +1,9 @@
+public class Loop2 {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 1; i <= 3; i++) {
+            System.out.println("I am so smart " + i);
+        }
+        System.out.println(i);
+    }
+}
