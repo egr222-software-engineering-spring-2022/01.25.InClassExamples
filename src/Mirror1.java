b4ca0e364865d70679aeeca69e0d81f5f4f6e7b0
Larry Clement <lclement@calbaptist.edu>
2022-01-24 17:13:57 -0800

    Minor change

---
+++ src/Mirror1.java
@@ -0,0 +1,20 @@
+public class Mirror1 {
+    public static final int SIZE = 4;
+
+    public static void main(String[] args) {
+        topHalf();
+        bottomHalf();
+    }
+
+    public static void topHalf() {
+        for (int i = 1; i <= SIZE; i++) {
+
+        }
+    }
+
+    public static void bottomHalf() {
+        for (int i = 1; i <= SIZE; i++) {
+
+        }
+    }
+}
