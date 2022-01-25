08b6ece35ce429f979490098c3651c665263b30d
Larry Clement <lclement@calbaptist.edu>
2022-01-24 17:14:43 -0800

    Delete Loop*.java

--- src/Loop1.java
+++
@@ -1,8 +0,0 @@
-public class Loop1 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("I am so smart " + i);
-        }
-//         System.out.println(i);
-    }
-}
